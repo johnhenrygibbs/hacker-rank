d312de7f65887fe289a62d29c82d536825b1b58d
John Gibbs <hello@johnhenrygibbs.com>
2022-09-24 22:38:24 -0700

    Passed sample test cases.

---
+++ 3 Months Preparation Kit/Week 1/Time Conversion/index.js
@@ -0,0 +1,36 @@
+let sampleTestCase0 = "07:05:45PM";
+
+function timeConversion(s) {
+
+    let militaryTime = "";
+    let array = s.split(":");
+
+    // Edge cases handle 12 AM conversion, 12 PM conversion, and military time conversion after noon. If no conversion is needed, "AM" is removed upon return.
+
+    if (array[0] == "12" && array[2].includes("AM")) {
+
+        array[0] = "00";
+        militaryTime = array.join(":").replace("AM", "");
+
+    } else if (array[0] == "12" && array[2].includes("PM")) {
+
+        militaryTime = array.join(":").replace("PM", "");
+
+    } else if (Number(array[0]) < 12 && array[2].includes("PM")) {
+
+        let number = Number(array[0]);
+        number += 12;
+        array[0] = number.toString();
+        militaryTime = array.join(":").replace("PM", "");
+
+    } else {
+
+        militaryTime = array.join(":");
+
+    }
+
+    return militaryTime.replace("AM", "");
+
+}
+
+console.log(timeConversion(sampleTestCase0));
