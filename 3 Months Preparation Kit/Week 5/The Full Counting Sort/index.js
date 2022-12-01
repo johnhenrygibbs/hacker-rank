5fabd034600de9f7a77d399d99f74d79f4ad7f40
John Gibbs <hello@johnhenrygibbs.com>
2022-11-30 20:15:15 -0800

    Passed sample test cases.

---
+++ 3 Months Preparation Kit/Week 5/The Full Counting Sort/index.js
@@ -0,0 +1,37 @@
+let arr = [[0, 'a'], [1, 'b'], [0, 'c'], [1, 'd']];
+
+// The instructions on this are really unclear, but the function can basically be broken into 3 parts.
+
+function countSort(arr) {
+
+    // Before sorting, iterate through the first half of the array and replace all strings with dashes.
+
+    for (let i = 0; i < arr.length / 2; i++) {
+
+        arr[i][1] = '-';
+
+    }
+
+    // Next we can sort the array based on the integer in the first half of each pairing.
+
+    arr.sort(function(a, b) {
+
+        return a[0] - b[0];
+
+    });
+
+    // Lastly, we create an empty string to hold our new sorted string and concatenate with spaces.
+
+    let sortedStrings = "";
+
+    for (let i = 0; i < arr.length; i++) {
+
+        sortedStrings += arr[i][1] + " ";
+
+    }
+
+    console.log(sortedStrings);
+
+}
+
+console.log(countSort(arr));
