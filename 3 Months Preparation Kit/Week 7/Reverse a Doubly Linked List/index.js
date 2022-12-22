766802c215e548cc3199ad7e82d5eb47ccb849d2
John Gibbs <hello@johnhenrygibbs.com>
2022-12-21 18:08:49 -0800

    Passed sample test cases.

--- 3 Months Preparation Kit/Week 7/Reverse a Doubly Linked List/index.js
+++ 3 Months Preparation Kit/Week 7/Reverse a Doubly Linked List/index.js
@@ -1,6 +1,6 @@
 function reverse(head) {
 
-    // This is basically the same as reversing a linked list, except that now you have to set previous and next pointers, since each node contains both.
+    // This is basically the same as reversing a linked list, except that now you have to set previous and next pointers since each node contains both.
 
     let temp = null;
 
