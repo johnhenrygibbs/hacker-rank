d7e03e29ae8d275eebddab1d30f375fcce38d7f0
John Gibbs <hello@johnhenrygibbs.com>
2022-12-22 10:44:54 -0800

    Passed sample test cases.

---
+++ 3 Months Preparation Kit/Week 7/Insert a Node at a Specific Position in a Linked List/index.js
@@ -0,0 +1,23 @@
+function insertNodeAtPosition(head, data, position) {
+
+    let node = new SinglyLinkedListNode(data);
+    let count = 0;
+    let temp = head;
+
+    // Begin by setting temp to head and incrementing its position along with the counter.
+
+    while (count < position - 1) {
+
+        temp = temp.next;
+        count++;
+
+    }
+
+    // Once the loop completes set next node and temp values and return head.
+
+    node.next = temp.next;
+    temp.next = node;
+
+    return head;
+
+}
