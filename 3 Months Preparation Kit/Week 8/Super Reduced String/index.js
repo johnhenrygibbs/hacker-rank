2ad40a86d9d105e702a86663d6e6e7fe26917e67
John Gibbs <hello@johnhenrygibbs.com>
2023-01-04 10:16:27 -0800

    Passes sample test cases.

---
+++ 3 Months Preparation Kit/Week 8/Super Reduced String/index.js
@@ -0,0 +1,44 @@
+let s = "abba";
+
+function superReducedString(s) {
+
+    let array = s.split("");
+    let index = 0;
+
+    // Split the string into an array and use a while loop to iterate through its contents.
+
+    while (index < array.length) {
+
+        let first = array[index];
+        let second = array[index + 1];
+
+        // Compare two elements at a time. Splice them if they are equal and reset the index. Otherwise, increment the index.
+
+        if (first === second) {
+
+            array.splice(index, 2);
+            index = 0;
+
+        } else {
+
+            index++;
+
+        }
+
+    }
+
+    // Conditional that checks to see if the array is empty or contains no dupes.
+
+    if (array.length === 0) {
+
+        return "Empty String";
+
+    } else {
+
+        return array.join("");
+
+    }
+
+}
+
+console.log(superReducedString(s));
