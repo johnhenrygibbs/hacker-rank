5d770a92b0ee2b9b2fbda124ff9b6dc213f7d1ba
John Gibbs <hello@johnhenrygibbs.com>
2024-02-01 20:24:50 -0800

    Passed sample test cases.

--- 3 Months Preparation Kit/Week 7/Goodland Electricity/index.js
+++ 3 Months Preparation Kit/Week 7/Goodland Electricity/index.js
@@ -1,11 +1,49 @@
 let k = 3;
-let arr = [0, 1, 1, 1, 0, 0, 0];
+let arr = [0, 1, 0, 0, 0, 1, 1, 1, 1, 1];
+
+// Question may be bugged according to discussion board. Passing solution below with attempted logic. 
 
 function pylons(k, arr) {
 
-    
+    let plants = 0;
+    let diameter = k * 2 - 1;
+    let last = -k;
+    let planned = -k;
 
-    return - 1;
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] === 1) {
+
+            planned = i;
+
+        }
+
+        let maxCoverage = last + diameter;
+
+        if (maxCoverage === i) {
+
+            if (planned !== last) {
+
+                plants++;
+                last = planned;
+
+            } else {
+
+                return -1;
+
+            }
+
+        }
+
+    }
+
+    if (planned !== last) {
+
+        plants++;
+
+    }
+
+    return plants;
 
 }
 
---
+++ 3 Months Preparation Kit/Week 7/Inserting a Node Into a Sorted Doubly Linked List/index.js
@@ -0,0 +1,57 @@
+function sortedInsert(head, data) {
+
+    let node = new DoublyLinkedListNode(data);
+    let temp = head;
+
+    // While the value of temp node is less than the value of the input data progress temp node.
+
+    while (temp.data < data) {
+
+        // If you reach the end of the linked list, break the while loop.
+
+        if (temp.next == null) {
+
+            break;
+
+        } else {
+
+            temp = temp.next;
+
+        }
+
+    }
+
+    // First case handles insertions before the head of the linked list.
+
+    if (temp.prev == null) {
+
+        temp.prev = node;
+        node.next = temp;
+        return node;
+
+    // Second case handles insertions in the middle of the linked list.
+
+    } else if (temp.data >= data) {
+
+        let before = temp.prev
+        let after = temp
+
+        before.next = node;
+        node.prev = before;
+
+        after.prev = node;
+        node.next = after;
+
+        return head;
+
+    // Third case handles insertions after the tail of the linked list.
+
+    } else {
+
+        temp.next = node;
+        node.prev = temp;
+        return head;
+
+    }
+
+}
